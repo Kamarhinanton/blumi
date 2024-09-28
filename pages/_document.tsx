5207bbe7bac1273632b15a151e6a2accf0fa9c6a
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-09-28 19:51:09 +0300

    feat: add icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon/favicon-16x16.png"
+        />
+      </Head>
       <body>
         <div className="wrapper">
           <Main />
