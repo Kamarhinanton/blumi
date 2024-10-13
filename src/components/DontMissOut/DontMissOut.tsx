aba456b30fee7f3b55794132e5b89c7bd67a5c1d
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-10-13 16:52:05 +0300

    feat: dontMissOut desktop

--- src/components/DontMissOut/DontMissOut.tsx
+++ src/components/DontMissOut/DontMissOut.tsx
@@ -1,6 +1,11 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { QueryResultDontMissOutData } from '@/components/DontMissOut/utils/types'
 import Container from '@/app/layouts/layouts/Container'
+import Heading from '@/components/Heading/Heading'
+import { cleanedTitleWithIcons } from '@/utils/global'
+import ButtonPrimary from '@/ui/ButtonPrimary/ButtonPrimary'
+
+import Icon from '../../../public/icons/light.svg'
 
 import styles from './DontMissOut.module.scss'
 
@@ -9,14 +14,28 @@ type DontMissOutType = {
 }
 
 const DontMissOut: FC<DontMissOutType> = ({ dontMissOut }) => {
-  useEffect(() => {
-    console.log(dontMissOut)
-  }, [])
+  const { title, buttonText } = dontMissOut
+  const { titleWithIcons, description, subText } = title
+  const cleanedData = cleanedTitleWithIcons(titleWithIcons || [])
 
   return (
     <section className={styles['dontMissOut']}>
       <Container size={'small'}>
-        <h1>hello</h1>
+        <div className={styles['dontMissOut__content']}>
+          <Heading
+            description={description || ''}
+            titleIcon={cleanedData}
+            tag={'h2'}
+            subText={subText || ''}
+            small
+            centred
+          >
+            <Icon />
+          </Heading>
+          <ButtonPrimary className={styles['button']}>
+            {buttonText}
+          </ButtonPrimary>
+        </div>
       </Container>
     </section>
   )
