8abd197ea6de654f6c1154dc9581100e4b24773b
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-10-13 21:08:41 +0300

    feat: dontMissOut mob

--- src/components/DontMissOut/DontMissOut.tsx
+++ src/components/DontMissOut/DontMissOut.tsx
@@ -22,19 +22,27 @@ const DontMissOut: FC<DontMissOutType> = ({ dontMissOut }) => {
     <section className={styles['dontMissOut']}>
       <Container size={'small'}>
         <div className={styles['dontMissOut__content']}>
-          <Heading
-            description={description || ''}
-            titleIcon={cleanedData}
-            tag={'h2'}
-            subText={subText || ''}
-            small
-            centred
-          >
-            <Icon />
-          </Heading>
-          <ButtonPrimary className={styles['button']}>
-            {buttonText}
-          </ButtonPrimary>
+          <div className={styles['bg-container']}>
+            <div className={styles['gradient-1']} />
+            <div className={styles['gradient-2']} />
+            <div className={styles['gradient-3']} />
+            <div className={styles['gradient-4']} />
+          </div>
+          <div className={styles['dontMissOut__content_text']}>
+            <Heading
+              description={description || ''}
+              titleIcon={cleanedData}
+              tag={'h2'}
+              subText={subText || ''}
+              small
+              centred
+            >
+              <Icon />
+            </Heading>
+            <ButtonPrimary className={styles['button']}>
+              {buttonText}
+            </ButtonPrimary>
+          </div>
         </div>
       </Container>
     </section>
