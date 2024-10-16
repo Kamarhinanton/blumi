538e83ec0a6b119afd4f98eedcaf1e386e7de247
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-10-16 16:22:54 +0300

    fix: [2024101516][2]

--- src/modules/Home/ui/HowItWorks/HowItWorks.tsx
+++ src/modules/Home/ui/HowItWorks/HowItWorks.tsx
@@ -34,7 +34,6 @@ const swiperProps: SwiperProps = {
     [breakpointMob + 1]: {
       spaceBetween: 24,
       centeredSlides: true,
-      slidesPerView: 2,
       allowTouchMove: false,
     },
   },
